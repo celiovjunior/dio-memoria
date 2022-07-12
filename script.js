fdbd46ddd90124e18bc1dc450a2c1df64c640063
celiovjunior <celio_cjr@hotmail.com>
2022-07-11 22:18:06 -0300

    made the function to reset the values of the variables

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let firstCard, secondCard;
 
 function flipCard() {
   if(lockBoard) return;
+  if(this === firstCard) return;
 
   this.classList.add('flip');
   
@@ -32,6 +33,8 @@ function checkForMatch() {
 function disableCards() {
   firstCard.removeEventListener('click', flipCard);
   secondCard.removeEventListener('click', flipCard);
+
+  resetBoard();
 }
 
 function unflipCards() {
@@ -41,12 +44,15 @@ function unflipCards() {
     firstCard.classList.remove('flip');
     secondCard.classList.remove('flip');
     
-    lockBoard = false;
+    resetBoard();
 
   }, 1500);
 }
 
-
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
 
 cards.forEach((card) => {
   card.addEventListener('click', flipCard);
